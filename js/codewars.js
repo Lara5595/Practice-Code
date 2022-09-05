3ed20f72b3fd9f394cc81a236c96838578ac1ca2
David Lara <david.a.lara05@gmail.com>
2022-09-04 21:06:09 -0500

    finished second

--- js/codewars.js
+++ js/codewars.js
@@ -1,5 +1,25 @@
+//first question
 function greet(message) {
     return "hello world";
 }
 
 console.log("Hello my name is david and " + greet());
+console.log(greet());
+
+// end of first question
+
+// second question
+function firstNonConsecutive (arr) {
+    for (let i=0; i < arr.length -1; i++) {
+        let curr = arr[i];
+        let next = arr[i + 1];
+
+        if (curr + 1 !== next) {
+            return next;
+        }
+    }
+    return null;
+}
+
+console.log(firstNonConsecutive);
+// end of question
