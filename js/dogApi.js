1a2b6887a21c95e74aa8e64ca1347a4454ca373c
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2022-12-04 14:45:33 -0600

    linked files and added api

--- js/dogApi.js
+++ js/dogApi.js
@@ -3,8 +3,24 @@
 // .then - Promises
 // asynchronous programming
 
+// stuff you don't have to wait for
+console.log('run 1st')
 
+const dogImageDiv = document.getElementById('dogImage')
+const dogButton = document.getElementById('dogButton')
+
+// stuff you have to wait for
+// I wrapped it in a function so we can use the button
+const getNewDog = () => {
 fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
-.then(json => console.log(json))
+.then(json => {
+ console.log(json.message)
+dogImageDiv.innerHTML = `<img src='${json.message}' height=300 width=300/>`
+})
+}
     
+console.log('run 3rd')
+
+
+dogButton.onclick = () => getNewDog();
