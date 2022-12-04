46b783e2aaa74af6f0fd4ab0fed86a01ce8278b0
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2022-12-04 16:39:01 -0600

    added random button and added extension so i need to restart

--- js/superheroAPI.js
+++ js/superheroAPI.js
@@ -1,18 +1,34 @@
 const SUPERHERO_TOKEN = SUPERHERO_TOKEN_API;
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`;
 
+const newHeroButton = document.getElementById('newHeroButton')
 
+const heroImageDiv = document.getElementById('heroImage')
+
+// This is a fetch api and get did a function for it so we can use it 
 const getSuperHero = (id, name) => {
+    //  When we search a hero it would be
+    //  name => base_url/search/batman
+    //  id => base_url/id
+    // json.results[0].image.url will get you the first result of the array since there will be mul
+
     fetch(`${BASE_URL}/${id}`)
     .then(response => response.json())
     .then(json => {
         console.log(json)
-        document.querySelector('body').innerHTML += `<img src="${json.image.url}" height=200 width=200/>`
+        heroImageDiv.innerHTML = `<img src="${json.image.url}" height=200 width=200/>`
     })
     }
 
-getSuperHero(20);
 
-const img = "https://www.superherodb.com/pictures2/portraits/10/100/10476.jpg"
+const getSearchSuperHero
 
+
+//  This makes it so you can get a random hero
+const randomHero = () => {
+    const numberofHeros = 731
+   return Math.floor(Math.random() * numberofHeros) + 1    
+}
+
+newHeroButton.onclick = () => getSuperHero(randomHero())
 
