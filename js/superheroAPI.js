f0fe06ebe0ae0bf420a0b6c26519b8cc7ed595b4
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2022-12-04 15:25:33 -0600

    created key and token vars

--- js/superheroAPI.js
+++ js/superheroAPI.js
@@ -1,5 +1,18 @@
-const getSuperHero = () => {
-    
-}
+const SUPERHERO_TOKEN = SUPERHERO_TOKEN_API;
+const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`;
 
-getSuperHero();
+
+const getSuperHero = (id, name) => {
+    fetch(`${BASE_URL}/${id}`)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json)
+        document.querySelector('body').innerHTML += `<img src="${json.image.url}" height=200 width=200/>`
+    })
+    }
+
+getSuperHero(20);
+
+const img = "https://www.superherodb.com/pictures2/portraits/10/100/10476.jpg"
+
+
