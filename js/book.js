24705fcb0b4981fc586e5a210116ce63bec3d1e5
David Lara <david.a.lara05@gmail.com>
2022-12-08 14:40:24 -0600

    added jquery and did ajax

--- js/book.js
+++ js/book.js
@@ -1,5 +1,21 @@
 function bookSearch(){
-    console.log('this function runs!');
+    // . value gets what the user typed
+    const search = document.getElementById('search').value
+    document.getElementById('results').innerHTML = ""
+    console.log(search)
+
+    $.ajax({
+        // Plus search adds what the user search on the search bar
+        url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+        dataType: "json",
+
+        success: function (data) {
+            console.log(data)
+        },
+
+        type: 'GET'
+    });
+
 }
 
 document.getElementById('button').addEventListener('click', bookSearch, false)
